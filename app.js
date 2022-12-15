d1dd9b6661114807ca60c9cf9533b1bad7eacbc7
byykkonen <118409410+byykkonen@users.noreply.github.com>
2022-12-15 14:41:06 -0600

    Corrected Exercise

--- app.js
+++ app.js
@@ -4,43 +4,54 @@ console.log(
 );
 
 // Exercise 1
+let a = 20;
+let b = 4;
+let sum = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
 
-const firstName = "Rhysand";
-let lastName = "Byykkonen";
-var age = 35;
+console.log(`${sum}`);
+console.log(`${minus}`);
+console.log(`${multiply}`);
+console.log(`${dividing}`);
+//results: 24 16 80 5
+
+
+// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
 
 // Exercise 2
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
+//results 1111, 11eleven, 12, Frodo11, true11, FrodoBaggins
 
-//Cannot not re-assign firstname;
-lastName = "Archeron";
-age = 21;
 
-
-
+// YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
 // Exercise 3
+console.log(val == str3);
+console.log(val === str3);
+console.log(!isAwake);
+console.log("eleven" == str4 && val >= str3);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 !== false);
+console.log(0 != false);
+console.log(0 !== false);
+//Results: true, false, true, false, true, false, true, false, true
 
-let language = "JavaScript";
-let createdYear = 1995 ;
-let isCaseSensitive = true;
 
-console.log(`${language} was released in ${createdYear}`);
-// Exercise 4
-
-let price = 19.99;
-let isOnSale = false;
-let salePercentage = 15;
-let stock = false;
-let inStock = false;
-let selectedSize = "M";
-
-console.log(`The shirt is ${price} is it on sale= ${stock}?`);
-
-// Exercise 5
-
-let title = "Name of the Wind";
-let author = "Patrick Rothfuss";
-let pageCount = 722;
-let bookmark = 456;
-let hasRead = true;
-
-console.log(`I am reading ${title}. I like the author ${author}. I am on page ${bookmark}!!`);
+// YOUR CODE HERE
